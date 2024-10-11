31516e451ab978fdf8b883631373ed1c920bc620
Allison <Allison.Boone@keyin.com>
2024-10-11 20:34:57 -0230

    Create getQuestion function

--- utils/mathUtilities.js
+++ utils/mathUtilities.js
@@ -3,7 +3,22 @@
  *
  * @returns {} The randomly generated math question
  */
-function getQuestion() {}
+function getQuestion() {
+  const operations = ['+', '-', '*', '/'];
+  const randomOperations =
+    operations[Math.floor(Math.random() * operations.length)];
+  const num1 = Math.floor(Math.random() * 100);
+  const num2 = Math.floor(Math.random() * 100);
+
+  if (randomOperations === '/' && num2 === 0) {
+    return getQuestion();
+  }
+
+  const question = `${num1} ${randomOperations} ${num2}`;
+  const answer = eval(question);
+
+  return question, answer;
+}
 
 /**
  * Parses the provided question and gets whether or not the provided answer is correct
