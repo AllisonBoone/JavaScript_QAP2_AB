e01c5b48bde64bb73fe40edc23f15ebdfe9622cd
Allison <Allison.Boone@keyin.com>
2024-10-11 21:45:03 -0230

    Fixed logic errors in getQuestion function

--- utils/mathUtilities.js
+++ utils/mathUtilities.js
@@ -10,13 +10,23 @@ function getQuestion() {
   const num1 = Math.floor(Math.random() * 100);
   const num2 = Math.floor(Math.random() * 100);
 
-  if (randomOperations === '/' && num2 === 0) {
-    return getQuestion();
+  let answer;
+  switch (randomOperations) {
+    case '+':
+      answer = num1 + num2;
+      break;
+    case '-':
+      answer = num1 - num2;
+      break;
+    case '*':
+      answer = num1 * num2;
+      break;
+    case '/':
+      answer = num2 !== 0 ? num1 / num2 : 0;
+      break;
   }
 
   const question = `${num1} ${randomOperations} ${num2}`;
-  const answer = eval(question);
-
   return { question, answer };
 }
 
@@ -28,7 +38,24 @@ function getQuestion() {
  * @returns {boolean} True if the answer was correct, false otherwise.
  */
 function isCorrectAnswer(question, answer) {
-  const correctAnswer = eval(question);
+  const [num1, operator, num2] = question.split(' ');
+  let correctAnswer;
+
+  switch (operator) {
+    case '+':
+      correctAnswer = parseInt(num1) + parseInt(num2);
+      break;
+    case '-':
+      correctAnswer = parseInt(num1) - parseInt(num2);
+      break;
+    case '*':
+      correctAnswer = parseInt(num1) * parseInt(num2);
+      break;
+    case '/':
+      correctAnswer = parseInt(num1) / parseInt(num2);
+      break;
+  }
+
   return (
     Math.round(correctAnswer * 100) / 100 === Math.round(answer * 100) / 100
   );
