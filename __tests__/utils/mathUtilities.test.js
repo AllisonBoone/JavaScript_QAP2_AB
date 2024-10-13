ecfb60ee844c498e781ca902953f45bb8875f763
Allison <Allison.Boone@keyin.com>
2024-10-12 23:03:38 -0230

    successfully made second test

--- __tests__/utils/mathUtilities.test.js
+++ __tests__/utils/mathUtilities.test.js
@@ -8,4 +8,10 @@ describe('Test for getQuestion', () => {
   });
 });
 
-describe('Tests for isCorrectAnswer', () => {});
+describe('Tests for isCorrectAnswer', () => {
+  test('return true for correct answer', () => {
+    const question = '1 + 1';
+    const answer = 2;
+    expect(isCorrectAnswer(question, answer)).toBe(true);
+  });
+});
